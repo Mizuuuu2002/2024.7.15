f4e56bae97f8da9ee58697f3ed7384fb4c4be5b0
Mizuuuu2002 <147463403+Mizuuuu2002@users.noreply.github.com>
2024-07-16 22:50:57 +0800

    Update simd_util.cpp

--- deps/common/math/simd_util.cpp
+++ deps/common/math/simd_util.cpp
@@ -13,62 +13,57 @@ See the Mulan PSL v2 for more details. */
 
 #if defined(USE_SIMD)
 
-/**
- * ?__m256i?????????????32????
- * 
- * ???????SSE4.1???????256???????????????32????
- * ?????????????AVX??????????????????
- * 
- * @param vec ??__m256i????????????32????
- * @param i ??????????????????????
- * @return ????????32????
- */
 int mm256_extract_epi32_var_indx(const __m256i vec, const unsigned int i)
 {
-  // ???i?????128??????????_mm256_permutevar8x32_epi32???
   __m128i idx = _mm_cvtsi32_si128(i);
-  
-  // ?????????????256??????????????????????
   __m256i val = _mm256_permutevar8x32_epi32(vec, _mm256_castsi128_si256(idx));
-  
-  // ?256??????128?????????32?????????
-  // ????_mm256_permutevar8x32_epi32????????????????????????
   return _mm_cvtsi128_si32(_mm256_castsi256_si128(val));
 }
 
 int mm256_sum_epi32(const int *values, int size)
-{ //code 
-  int sum = 0;
-  for (int i = 0; i < size / 8 * 8; i += 8) {
-    __m256i v = _mm256_loadu_si256((__m256i*)(values + i));
-    v = _mm256_hadd_epi32(v, v);
-    v = _mm256_hadd_epi32(v, v);
-    int temp[8];
-    _mm256_storeu_si256((__m256i*)temp, v);
-    sum += temp[0] + temp[1] + temp[2] + temp[3];
+{
+  if(size<16){
+    int sum=0;
+    for(int i=0;i<size;i++){
+      sum+=values[i];
+    }
+    return sum;
   }
-  for (int i = size / 8 * 8; i < size; i++) {
-    sum += values[i];
+  int epoch=size/16;//????
+  int temp[epoch*8+size-epoch*16];//?????
+  for(int j=0 ; j<epoch ; j++){
+    __m256i temp1 = _mm256_loadu_epi32(values[0+j*16]);//?8?
+    __m256i temp2 = _mm256_loadu_epi32(values[0+j*16+8]);//?8?
+    __m256i temp3  = _mm256_add_epi32(temp1, temp2);
+    _mm256_storeu_epi32(temp[0+j*8], temp3);//??????
   }
-  return sum;
+  for(int i=epoch*16,j=0;i<size;i++,j++){
+    temp[j+epoch*8]=value[i];
+  }
+  mm256_sum_epi32(temp,epoch*8+size-epoch*16);
 }
 
 float mm256_sum_ps(const float *values, int size)
 {
-  //code
-  float sum = 0.0f;
-  for (int i = 0; i < size / 8 * 8; i += 8) {
-    __m256 v = _mm256_loadu_ps(values + i);
-    v = _mm256_hadd_ps(v, v);
-    v = _mm256_hadd_ps(v, v);
-    float temp[8];
-    _mm256_storeu_ps(temp, v);
-    sum += temp[0] + temp[1] + temp[2] + temp[3];
+  if(size<16){
+    float sum=0;
+    for(int i=0;i<size;i++){
+      sum+=values[i];
+    }
+    return sum;
   }
-  for (int i = size / 8 * 8; i < size; i++) {
-    sum += values[i];
+  int epoch=size/16;//????
+  float temp[epoch*8+size-epoch*16];//?????
+  for(int j=0 ; j<epoch ; j++){
+    __m256 temp1 = _mm256_loadu_ps(values[0+j*16]);//?8?
+    __m256 temp2 = _mm256_loadu_ps(values[0+j*16+8]);//?8?
+    __m256 temp3  = _mm256_add_ps(temp1, temp2);
+    _mm256_storeu_ps(temp[0+j*8], temp3);//??????
   }
-  return sum;
+  for(int i=epoch*16,j=0;i<size;i++,j++){
+    temp[j+epoch*8]=value[i];
+  }
+  mm256_sum_epi32(temp,epoch*8+size-epoch*16);
 }
 
 template <typename V>
