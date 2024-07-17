68d2900bcc42b3781ac112bc26d11f7323084ab3
Mizuuuu2002 <147463403+Mizuuuu2002@users.noreply.github.com>
2024-07-17 16:25:49 +0800

    Update simd_util.cpp

--- deps/common/math/simd_util.cpp
+++ deps/common/math/simd_util.cpp
@@ -20,51 +20,64 @@ int mm256_extract_epi32_var_indx(const __m256i vec, const unsigned int i)
   return _mm_cvtsi128_si32(_mm256_castsi256_si128(val));
 }
 
-int mm256_sum_epi32(const int *values, int size)
-{
-  if(size<16){
-    int sum=0;
-    for(int i=0;i<size;i++){
-      sum+=values[i];
+int mm256_sum_epi32(const int *values, int size) {
+    if (size < 16) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += values[i];
+        }
+        return sum;
     }
-    return sum;
-  }
-  int epoch=size/16;//????
-  int temp[epoch*8+size-epoch*16];//?????
-  for(int j=0 ; j<epoch ; j++){
-    __m256i temp1 = _mm256_loadu_epi32(&values[0+j*16]);//?8?
-    __m256i temp2 = _mm256_loadu_epi32(&values[0+j*16+8]);//?8?
-    __m256i temp3  = _mm256_add_epi32(temp1, temp2);
-    _mm256_storeu_epi32(temp[0+j*8], temp3);//??????
-  }
-  for(int i=epoch*16,j=0;i<size;i++,j++){
-    temp[j+epoch*8]=value[i];
-  }
-  mm256_sum_epi32(temp,epoch*8+size-epoch*16);
+
+    int epoch = size / 16; // 处理16的倍数部分
+    int temp_size = epoch * 8 + size - epoch * 16; // 临时数组的大小
+    int *temp = new int[temp_size]; // 动态分配临时数组
+
+    for (int j = 0; j < epoch; j++) {
+        __m256i temp1 = _mm256_loadu_si256((__m256i*)&values[j * 16]); // 载入前8个整数
+        __m256i temp2 = _mm256_loadu_si256((__m256i*)&values[j * 16 + 8]); // 载入后8个整数
+        __m256i temp3 = _mm256_add_epi32(temp1, temp2);
+        _mm256_storeu_si256((__m256i*)&temp[j * 8], temp3); // 存储相加结果
+    }
+
+    for (int i = epoch * 16, j = 0; i < size; i++, j++) {
+        temp[j + epoch * 8] = values[i];
+    }
+
+    int result = mm256_sum_epi32(temp, temp_size); // 递归计算剩余部分的和
+    delete[] temp; // 释放动态数组
+    return result;
 }
 
-float mm256_sum_ps(const float *values, int size)
-{
-  if(size<16){
-    float sum=0;
-    for(int i=0;i<size;i++){
-      sum+=values[i];
+float mm256_sum_ps(const float *values, int size) {
+    if (size < 16) {
+        float sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += values[i];
+        }
+        return sum;
     }
-    return sum;
-  }
-  int epoch=size/16;//????
-  float temp[epoch*8+size-epoch*16];//?????
-  for(int j=0 ; j<epoch ; j++){
-    __m256 temp1 = _mm256_loadu_ps(&values[0+j*16]);//?8?
-    __m256 temp2 = _mm256_loadu_ps(&values[0+j*16+8]);//?8?
-    __m256 temp3  = _mm256_add_ps(temp1, temp2);
-    _mm256_storeu_ps(temp[0+j*8], temp3);//??????
-  }
-  for(int i=epoch*16,j=0;i<size;i++,j++){
-    temp[j+epoch*8]=value[i];
-  }
-  mm256_sum_epi32(temp,epoch*8+size-epoch*16);
+
+    int epoch = size / 16; // 处理16的倍数部分
+    int temp_size = epoch * 8 + size - epoch * 16; // 临时数组的大小
+    float *temp = new float[temp_size]; // 动态分配临时数组
+
+    for (int j = 0; j < epoch; j++) {
+        __m256 temp1 = _mm256_loadu_ps(&values[j * 16]); // 载入前8个浮点数
+        __m256 temp2 = _mm256_loadu_ps(&values[j * 16 + 8]); // 载入后8个浮点数
+        __m256 temp3 = _mm256_add_ps(temp1, temp2);
+        _mm256_storeu_ps(&temp[j * 8], temp3); // 存储相加结果
+    }
+
+    for (int i = epoch * 16, j = 0; i < size; i++, j++) {
+        temp[j + epoch * 8] = values[i];
+    }
+
+    float result = mm256_sum_ps(temp, temp_size); // 递归计算剩余部分的和
+    delete[] temp; // 释放动态数组
+    return result;
 }
+
 template <typename V>
 void selective_load(V *memory, int offset, V *vec, __m256i &inv)
 {
